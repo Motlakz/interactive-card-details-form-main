74b41c664482453583793cc874a986e371525952
MOTLAKZ <motlalepulasello5@gmail.com>
2023-02-25 21:33:30 +0200

    main-changes: adjust styling according to screen width
    - added card components to the form
    - adjusted card and form content based on responsive screen sizes
    - created fireworks animation for form completed page
    - included javascript code for making the card components change position when specific inputs are completed

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.querySelector('.card-number-input').oninput = () => document.querySelector('.card-number').innerText = document.querySelector('.card-number-input').value;
+document.querySelector('.card-holder-input').oninput = () => document.querySelector('.cc-name').innerText = document.querySelector('.card-holder-input').value;
+document.querySelector('.month-input').oninput = () => document.querySelector('.exp-month').innerText = document.querySelector('.month-input').value;
+document.querySelector('.year-input').oninput = () => document.querySelector('.exp-year').innerText = document.querySelector('.year-input').value;
+document.querySelector('.cvv-input').onmouseenter = () => {
+  document.querySelector('.card-front').style.transform = 'perspective(1000px) rotateY(-180deg)';
+  document.querySelector('.card-back').style.transform = 'perspective(1000px) rotateY(0deg)';
+};
+document.querySelector('.cvv-input').onmouseleave = () => {
+  document.querySelector('.card-front').style.transform = 'perspective(1000px) rotateY(0deg)';
+  document.querySelector('.card-back').style.transform = 'perspective(1000px) rotateY(180deg)';
+};
+document.querySelector('.cvv-input').oninput = () => document.querySelector('.cvv-box').innerText = document.querySelector('.cvv-input').value;
